b71e17c5eca37e9d58d9bf53bb3822d7dfd8f02e
Ryan Hall <ryan.h@roadmunk.com>
2020-09-16 14:11:30 -0400

    cors and a mocked response

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const Koa = require('koa');
 const app = new Koa();
+const cors = require('@koa/cors');
 const db = require('./classifier');
 
 // application variables
 const PORT = 3000;
-
+app.use(cors());
 app.use(async ctx => {
   const {text, orgID } = ctx.request.query;
-  if (!orgID) ctx.throw(400, 'Missing mandatory param "orgID"');
-  if (!text) ctx.throw(400, 'Missing mandatory param "text"');
+  if (ctx.path === '/suggest') {
+    ctx.body = JSON.stringify({
+      idea: 'dummy idea label'
+    })
+  } else {
+    if (!orgID) ctx.throw(400, 'Missing mandatory param "orgID"');
+    if (!text) ctx.throw(400, 'Missing mandatory param "text"');
 
-  try {
-    const classifier = db.fetch(orgID);
-    const label = classifier.getClassifications(text);
-    ctx.body = label
-  } catch (err) {
-    ctx.throw(404, err.message);
+    try {
+      const classifier = db.fetch(orgID);
+      const label = classifier.getClassifications(text);
+      ctx.body = label
+    } catch (err) {
+      ctx.throw(404, err.message);
+    }
   }
+
 });
 
 app.listen(PORT);
